df268ae34d6874f073c437920d1c3a26cda9ac69
Mikhail Volzhanin <mrvolzhanin@yandex.ru>
2023-12-08 18:07:27 +0300

    added benchmark_sqlAlchemy

---
+++ benchmark_sqlAlchemy.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy import  Column, Integer, String
+from sqlalchemy.orm import DeclarativeBase
+import time
+
+engine = create_engine('sqlite:///yellow.db')
+
+def test(request):
+    results_time = []
+    
+    for i in range(10):
+        start = time.time() 
+        with engine.connect() as conn:
+            res = conn.execute(text(request))
+        results_time.append(time.time() - start)
+            
+    return round(sum(results_time)/len(results_time), 2)
+
+print("First SQL query: ")
+print(test("SELECT passenger_count, avg(total_amount) FROM yellow GROUP BY 1"))
+
+print("Second SQL query: ")
+print(test("SELECT passenger_count, avg(total_amount) FROM yellow GROUP BY 1"))
+
+print("Third SQL query: ")
+print(test("""
+        SELECT passenger_count, strftime('%Y', tpep_pickup_datetime) AS pickup_year, COUNT(*)
+        FROM yellow
+        GROUP BY passenger_count, pickup_year
+    """))
+
+print("Fourth SQL query: ")
+print(test("""SELECT passenger_count, 
+                    strftime('%Y', tpep_pickup_datetime) AS pickup_year,
+                    round(trip_distance),
+                    count(*)
+                    FROM yellow GROUP BY 1, 2, 3 ORDER BY 2, 4 DESC"""
+           ))
+
+
